063658aed9264de7d60cfd7c91f648c6c53f2f15
saber2001dz <saber2001dz@gmail.com>
2025-10-29 19:37:09 +0100

    correction titre page login

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -46,9 +46,9 @@ export function LoginForm({ className, ...props }: LoginFormProps) {
   return (
     <form onSubmit={handleSignIn} className={cn("flex flex-col gap-6", className)} dir="ltr" {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-4xl font-extrabold text-[#0E6681]  tracking-tight sm:text-4xl">Welcome Back</h1>
+        <h1 className="text-4xl font-extrabold text-[#0E6681] tracking-tight sm:text-4xl">Welcome Back</h1>
         <p className="text-muted-foreground text-sm max-w-[90%] sm:max-w-full">
-          Enter your email and password below to login to your account.
+          Saisissez votre adresse e-mail et votre mot de passe ci-dessous pour vous connecter à votre compte.
         </p>
       </div>
       <div className="grid gap-6">
@@ -68,7 +68,7 @@ export function LoginForm({ className, ...props }: LoginFormProps) {
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
             <a href="#" className="ml-auto text-sm underline-offset-4 hover:underline">
-              Forgot your password?
+              Mot de passe oublié?
             </a>
           </div>
           <Input
